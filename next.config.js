dab3043847f50be4e1e784d33425622ed4eeafd9
Gopalakrishna Panditi <gopalakrishnapanditi@gmail.com>
2026-07-22 21:09:37 +0100

    Fix workspace root warning via outputFileTracingRoot

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  outputFileTracingRoot: path.join(__dirname),
   images: {
     unoptimized: true,
   },
